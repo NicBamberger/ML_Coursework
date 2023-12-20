725584ce0b680ab6efbf75fa5298ff4603c991ce
Nic Bamberger <nic.bamberger@me.com>
2023-12-20 16:53:30 +0000

    Changed Grid Search to 5

--- ML_COURSEWORK/experiments/functions/pipeline.py
+++ ML_COURSEWORK/experiments/functions/pipeline.py
@@ -203,8 +203,8 @@ class Gx:
     def grid_search(features, target):
         
         # try 5 X 5
-        max_depth_list = np.linspace(3, 11, 2)
-        min_impurity_decrease_list = np.linspace(0, 0.1, 2)
+        max_depth_list = np.linspace(3, 11, 5)
+        min_impurity_decrease_list = np.linspace(0, 0.1, 5)
 
         n_hyperparamter_choices = len(max_depth_list) * len(min_impurity_decrease_list)
         all_errors = pd.DataFrame(index=range(n_hyperparamter_choices), 
